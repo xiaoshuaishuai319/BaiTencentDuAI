3c6f0d3f6edd96ba51652200b1c794d168ed3ffb
小帅丶 <578274480@qq.com>
2018-03-29 13:39:07 +0800

    :sparkles:新增百度AI图像识别-菜品识别-自定义图库入库、检索、删除API-Java示例代码

---
+++ src/main/java/cn/xsshome/baitencentdu/baiduai/imageclassify/dish/DishDeleteSample.java
@@ -0,0 +1,49 @@
+package cn.xsshome.baitencentdu.baiduai.imageclassify.dish;
+
+import java.net.URLEncoder;
+
+import cn.xsshome.baitencentdu.baiduai.util.Base64Util;
+import cn.xsshome.baitencentdu.baiduai.util.FileUtil;
+import cn.xsshome.baitencentdu.baiduai.util.HttpUtil;
+/**
+ * 自定义菜品删除
+ * @author 小帅丶
+ *
+ */
+public class DishDeleteSample {
+		//菜品删除接口
+		private static final String DISH_DELETE = "https://aip.baidubce.com/rest/2.0/image-classify/v1/realtime_search/dish/delete";
+		//更换为自己应用的AccessToken
+		static String token= "自己应用的AccessToken";
+		//调用示例代码
+		public static void main(String[] args) {
+			//入库的图片本地路径
+			String imagePath = "图片本地路径";
+			//默认传appid
+			String sub_lib="appid";
+			String result = DishSearch(imagePath, token, sub_lib);
+			System.out.println(new String(result));
+			//{"log_id": 2879203167506090135, "cont_sign": "100263660,3043140915"}
+		}
+		/**
+		 * 菜品检索
+		 * @param path 图片路径
+		 * @param token AccessToken
+		 * @param sub_lib 默认传appid
+		 * @return
+		 */
+		public static String DishSearch(String path,String token,String sub_lib) {
+			String result="";
+			try {
+	            String filePath = path;
+	            byte[] imgData = FileUtil.readFileByBytes(filePath);
+	            String imgStr = Base64Util.encode(imgData);
+	            String imgParam = URLEncoder.encode(imgStr, "UTF-8");
+	            String param = "image=" + imgParam+"&sub_lib="+sub_lib;
+	            result = HttpUtil.post(DISH_DELETE, token, param);
+	        } catch (Exception e) {
+	        	result = e.getMessage();
+	        }
+	        return result;
+	    }
+}
